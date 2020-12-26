3b4998881d020e9293958b3811fe9792977d89ff
Ujwal <ujwal99arak@gmail.com>
2020-12-26 15:17:04 +0530

    minor done with movie crud minor bugfixes remaining

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,51 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
+import {convertToken} from '../../utils'
+import {useLocalStorage} from '../../hooks'
 import Card from '../../components/Card';
 import Label from '../../components/Label';
 import Movie from '../Movie';
 import './styles.css';
+import Button from '../../components/Button';
 
 const Home = ({movieList}) => {
-  const [movieModal, setMovieModal] = useState();
+  const [movieModal, setMovieModal] = useState(false);
+  const [userData, setUserData] = useLocalStorage('token');
+  const [currentMovie, setCurrentMovie] = useState(null);
+  const [access, setAccess] = useState(false);
+
+  useEffect(() => {
+    const info = convertToken(userData?.token);
+    
+    if (info && info.access === 'ADMIN') {
+      setAccess(true)
+    }
+  }, []);
+
+  const handleClick = (item) => {
+    setMovieModal(true)
+    setCurrentMovie(item);
+  }
+
+  const handleClickMovieAdd = () => {
+    setMovieModal(true);
+    setCurrentMovie(null);
+  }
+
   return (
     <div className="home">
+      {access && 
+      <div className="add-container">
+        <Button onClick={handleClickMovieAdd}>ADD MOVIE</Button>
+      </div>
+      }
       <div className="movie-grid">
       {
         movieList.map((movie, index) => 
-          <Card title={movie.name} key={`${index}`}>
+          <Card 
+            title={movie.name} 
+            key={`${index}`} 
+            onClick={()=>handleClick(movie)}
+          >
             <p>by- {movie.director}</p>
             <p>IMDB score {movie.imdb_score}</p>
             <p>99popularity score {movie['99popularity']}</p>
@@ -21,7 +55,11 @@ const Home = ({movieList}) => {
           )
       }
       </div>
-      <Movie movieModal={movieModal} setMovieModal={setMovieModal}/>
+      {access && <Movie 
+        movieModal={movieModal} 
+        setMovieModal={setMovieModal}
+        currentMovie={currentMovie}  
+      />}
     </div>
   );
 }
--- src/pages/Movie/index.js
+++ src/pages/Movie/index.js
@@ -1,21 +1,79 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
+import {useLocalStorage} from '../../hooks';
 import Modal from '../../components/Modal';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
+import './styles.css';
 
-const Movie = ({movieModal, setMovieModal}) => {
-
-  const [mode, setMode] = useState('EDIT');
+const Movie = ({movieModal, setMovieModal, currentMovie}) => {
+  const mode = currentMovie ? 'EDIT' : 'ADD';
+  const [userData] = useLocalStorage('token');
   const [movieForm, setMovieForm] = useState({
     name: '',
     director: '',
     '99popularity': '',
     imdb_score: '',
   });
+  
+  useEffect(() => {
+    setMovieForm(currentMovie);
+  }, [currentMovie])
+
+  const headers = () => ({
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${userData?.token}`, 
+  });
+
+  const handleEdit = async() => {
+    try {
+      const raw = await fetch(`/api/movie/update/${currentMovie?._id}`, {
+        headers: headers(),
+        method: 'PUT',
+        body: JSON.stringify(movieForm), 
+      });
+      const data = await raw.json();
+      setMovieModal(false);
+      console.log(data);
+    } catch(error) {
+      console.log(error)
+    }
+  }
+
+  const handleAdd = async() => {
+    try {
+      const raw = await fetch(`/api/movie/add`, {
+        headers: headers(),
+        method: 'POST',
+        body: JSON.stringify(movieForm), 
+      });
+      const data = await raw.json();
+      setMovieModal(false);
+      console.log(data);
+    } catch(error) {
+      console.log(error)
+    } 
+  }
+
+  const handleDelete = async() => {
+    try {
+      const raw = await fetch(`/api/movie/delete/${currentMovie?._id}`, {
+        headers: headers(),
+        method: 'DELETE',
+      });
+      const data = await raw.json();
+      setMovieModal(false);
+      console.log(data);
+    } catch(error) {
+      console.log(error)
+    }
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (mode === 'ADD')  handleAdd();
+    if (mode === 'EDIT')  handleEdit();
   }
+
   const handleChange = (e) => {
     setMovieForm({
       ...movieForm,
@@ -26,6 +84,7 @@ const Movie = ({movieModal, setMovieModal}) => {
   const handleCancel = () => {
     setMovieModal(false);
   }
+  console.log(currentMovie);
   return (
     <Modal
       toggle={movieModal}
@@ -33,11 +92,12 @@ const Movie = ({movieModal, setMovieModal}) => {
     >
        <form onSubmit={handleSubmit}>
         <div className="movie-form">
-          <Input name="name" placeholder="name" value={movieForm.name} onChange={handleChange}/>
-          <Input name="director" placeholder="director" value={movieForm.director} onChange={handleChange}/>
-          <Input name="99popularity" placeholder="99popularity" value={movieForm.director} onChange={handleChange}/>
-          <Input name="imdb_score" placeholder="imdb_score" value={movieForm.director} onChange={handleChange}/>
+          <Input name="name" placeholder="name" value={movieForm?.name} onChange={handleChange}/>
+          <Input name="director" placeholder="director" value={movieForm?.director} onChange={handleChange}/>
+          <Input name="99popularity" placeholder="99popularity" value={movieForm?.['99popularity']} onChange={handleChange}/>
+          <Input name="imdb_score" placeholder="imdb_score" value={movieForm?.imdb_score} onChange={handleChange}/>
           <Button type="submit">{mode}</Button>
+          {mode === 'EDIT' && <Button onClick={handleDelete}>DELETE</Button>}
         </div>
         </form>
     </Modal>
